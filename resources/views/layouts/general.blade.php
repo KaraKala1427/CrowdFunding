6758ea4432a9df81daf8aed97ca18a698f8d2686
shaded92 <damir.shaltaev@gmail.com>
2021-12-19 19:19:06 +0600

    publications

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -6,8 +6,6 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
-    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
--- resources/views/pages/publications/publish.blade.php
+++ resources/views/pages/publications/publish.blade.php
@@ -5,36 +5,71 @@
 @endsection
 
 @section('content')
-    <section id="home" class="intro-section">
+    <section class="publish-section">
         <div class="container">
-        <h1>Добавление объявление</h1>
+        <div class="row text-center text-white">
+            <h1 class="display-3 fw-bold">Добавление публикации</h1>
+{{--            <hr style="width: 100px; height: 3px; " class="mx-auto">--}}
+        </div>
 
         <form action="#" method="post" enctype="multipart/form-data">
             @csrf
-            <div class="form-group">
-                <label for="name">Название</label>
-                <input type="text" id="name" name="name" class="form-control" placeholder="Название">
+
+            <div class="form-group mt-3">
+                <label for="category" class="text-white">Категория</label>
+                <select id="category" name="category" class="form-control">
+                    <option value="">Category 1</option>
+                    <option value="">Category 2</option>
+                    <option value="">Category 3</option>
+                </select>
             </div>
-            <div class="form-group">
-                <label for="description">Описание</label>
+
+            <div class="form-group mt-3">
+                <label for="name" class="text-white">Название проекта</label>
+                <input type="text" id="name" name="name" class="form-control" placeholder="Введите название">
+            </div>
+            <div class="form-group mt-3">
+                <label for="description" class="text-white">Описание</label>
                 <textarea name="description" id="description" class="form-control" placeholder="Описание"></textarea>
             </div>
-            <div class="form-group">
-                <label for="price">Цена</label>
-                <input type="number" id="price" name="price" class="form-control" placeholder="Цена">
-                @error('price')
-                <small id="emailHelp" class="form-text text-danger">Укажите цену</small>
-                @enderror
-            </div>
-            <div class="form-group">
-                <label for="img">Рисунок</label>
+{{--            <div class="form-group mt-3">--}}
+{{--                <label for="price" class="text-white">Цена</label>--}}
+{{--                <input type="number" id="price" name="price" class="form-control" placeholder="Цена">--}}
+{{--                @error('price')--}}
+{{--                <small id="emailHelp" class="form-text text-danger">Укажите цену</small>--}}
+{{--                @enderror--}}
+{{--            </div>--}}
+            <div class="form-group mt-3">
+                <label for="img" class="text-white">Рисунок</label>
                 <input type="file" class="form-control" id="img" name="img" accept="image/*"><br><br>
                 @error('img')
                 <small id="emailHelp" class="form-text text-danger">Загрузите рисунок</small>
                 @enderror
             </div>
-            <button type="submit" class="btn btn-success">Добавить запись</button>
+
+            <div class="row justify-content-center">
+                <a class="btn btn-light col-4 p-2"
+                   href="{{route('profile.publish')}}">Создать публикацию</a>
+            </div>
+
         </form>
         </div>
     </section>
+
+    <script src="https://cdn.tiny.cloud/1/o65lxpcmzh1b9m7amf72e0rvw485mdj8m5y6reaoe65r8z35/tinymce/5/tinymce.min.js"  referrerpolicy="origin"></script>
+    <script src="{{asset('filemanager2/js/tinymce/tinymce.min.js')}}" ></script>
+    <script>
+        window.onload = function () {
+            tinymce.init({
+                height: 250,
+                mode : "textareas",
+                plugins: "link image code table",
+                menubar: 'insert tools table',
+                toolbar: 'bold italic underline | link | image code | copy aligncenter alignjustify alignleft alignnone alignright',
+                image_caption: true,
+                file_browser_callback_types: 'file image media',
+                file_picker_callback: filemanager.tinyMceCallback,
+            });
+        };
+    </script>
 @endsection
